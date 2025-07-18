d76978168629942051f7f3cddca83515ddd3cf21
Enoch Baah <ebaah2942@gmail.com>
2025-07-18 15:08:42 +0000

    Added user model

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=[('host', 'Host'), ('guest', 'Guest')])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Listing(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
